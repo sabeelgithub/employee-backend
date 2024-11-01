dfc8e6a6a92ad7ec9d3dad546f221c2d635e6541
mohammedsabeel <mohammedsabeeltc786@gmail.com>
2024-11-01 23:32:06 +0530

    settings configuration change

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-at2jvh93kqa8)+23wif#&=th-$*19)!-p&spk+$bs2j3c5x6%!'
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
