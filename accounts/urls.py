04e3dea188deca4d54fd0d90fd2fbcb304bb41fc
mohammedsabeel <mohammedsabeeltc786@gmail.com>
2024-11-01 21:47:05 +0530

    slashes add

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('create/',RegisterView.as_view()),
     path('login/',Login.as_view()),
     path('token/refresh/', MyTokenRefreshView.as_view(), name='token_refresh'),
-    path('change-password', ChangePassword.as_view()),
-    path('profile', UserProfileData.as_view())
+    path('change-password/', ChangePassword.as_view()),
+    path('profile/', UserProfileData.as_view())
 
 ]
     
